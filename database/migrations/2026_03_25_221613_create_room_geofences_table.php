7cc63681c57e77edea7dc63cffa261cc10fa7471
Boh Dzomo Bob Peter <bohdzomobobpeter@gmail.com>
2026-03-25 23:22:33 +0100

    made the room_geofences migration

---
+++ database/migrations/2026_03_25_221613_create_room_geofences_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('room_geofences', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('room_id')->constrained()->onDelete('cascade');
+            $table->enum('shape_type', ['circle', 'polygon']);
+            $table->geography('boundary')->nullable(); // Stores Point for circle or Polygon
+            $table->decimal('radius', 10, 2)->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('room_geofences');
+    }
+};
